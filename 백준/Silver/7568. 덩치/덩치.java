6dff894cbc51ae9a565b7bd0b966dcae55621011
suuuinkim <78335072+suuuinkim@users.noreply.github.com>
2023-02-06 20:15:00 +0900

    [Silver V] Title: 덩치, Time: 252 ms, Memory: 18396 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Main m = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] arr = new int[n][2];
+
+        for(int i=0; i<n; i++){
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+        for(int i=0; i<n; i++){
+            int rank = 1;
+            for(int j=0; j<n; j++){
+
+                if(i == j) continue;
+
+                if(arr[i][0] < arr[j][0] && arr[i][1] <arr[j][1]){
+                    rank++;
+
+                }
+            }
+            System.out.print(rank + " ");
+        }
+
+
+
+
+
+    }
+}
