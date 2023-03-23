dd5448bbddc8ce88868dd40a019e3a5f923a8eb5
suuuinkim <78335072+suuuinkim@users.noreply.github.com>
2023-03-23 12:28:52 +0900

    [level 2] Title: 위장, Time: 0.11 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.java
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -1,23 +1,23 @@
 import java.util.*;
-import java.util.Map.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = clothes.length;
         HashMap<String, Integer> map = new HashMap<>();
         
         for(int i=0; i<clothes.length; i++){
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 1) + 1);
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 1)+1);
         }
         
-                
+        // System.out.println(map);
+        
         int sum = 1;
+        
         for(int x : map.values()){
-          sum *= x;  
+            sum *= x;
         }
         
-        answer = sum - 1;
-
-        
+        answer = sum -1;
+        // System.out.println(sum);
         return answer;
     }
 }
