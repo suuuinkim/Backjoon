d867cd174cff917c872cb8a6904a4ebb79c3d271
suuuinkim <78335072+suuuinkim@users.noreply.github.com>
2023-03-23 16:58:28 +0900

    [level 2] Title: 기능개발, Time: 0.30 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/README.md
+++ 프로그래머스/lv2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 73.5 MB, 시간: 0.33 ms
+메모리: 79.4 MB, 시간: 0.30 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -1,30 +1,26 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        
         Queue<Integer> q = new LinkedList<>();
-        List<Integer> answerList = new ArrayList<>();
-
-        for (int i = 0; i < speeds.length; i++) {
-            double remain = (100 - progresses[i]) / (double) speeds[i];
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<speeds.length; i++){
+            double remain = (100-progresses[i])/(double)speeds[i];
             int date = (int) Math.ceil(remain);
 
-            if (!q.isEmpty() && q.peek() < date) {
-                answerList.add(q.size());
+            if(!q.isEmpty() && q.peek() < date){
+                list.add(q.size());
                 q.clear();
             }
-
             q.offer(date);
         }
-
-        answerList.add(q.size());
-
-        int[] answer = new int[answerList.size()];
-
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = answerList.get(i);
+        
+        list.add(q.size());
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
         }
-
         return answer;
     }
 }
