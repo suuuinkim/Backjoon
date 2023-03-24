d69791d4fd28d56ebaeac682f4a036d091c8eb61
suuuinkim <78335072+suuuinkim@users.noreply.github.com>
2023-03-24 14:03:39 +0900

    [level 2] Title: H-Index, Time: 2.16 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -2,13 +2,21 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        // 내림차순 정렬
+        // 내림차순
+        
+        //citations = Arrays.stream(citations).boxed()
+        //.sorted(Collections.reverseOrder()).mapToInt(Integer::intValue).toArray();
+        
         Integer[] arr = Arrays.stream(citations).boxed().toArray(Integer[]::new);
         Arrays.sort(arr, Collections.reverseOrder());
+        // System.out.println(arr);
+        
+        
+        
         for(int i=0; i<arr.length; i++){
-            if(arr[i] >= i+1) answer++;
-            
+            if(arr[i] >= i + 1) answer++;
         }
+        
         return answer;
     }
 }
