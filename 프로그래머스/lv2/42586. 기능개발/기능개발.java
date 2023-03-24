cc19b42a31ae33a169b4deb1cde8f6d6ef816def
suuuinkim <78335072+suuuinkim@users.noreply.github.com>
2023-03-24 14:15:33 +0900

    [level 2] Title: 기능개발, Time: 0.29 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -2,22 +2,27 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         
+        
         Queue<Integer> q = new LinkedList<>();
         List<Integer> list = new ArrayList<>();
         
-        for(int i=0; i<speeds.length; i++){
+        for(int i=0; i<progresses.length; i++){
             double remain = (100-progresses[i])/(double)speeds[i];
-            int date = (int) Math.ceil(remain);
-
-            if(!q.isEmpty() && q.peek() < date){
+            int date = (int)Math.ceil(remain);
+            
+            if(!q.isEmpty() && q.peek()<date){
                 list.add(q.size());
                 q.clear();
             }
+            
             q.offer(date);
         }
         
         list.add(q.size());
+        
         int[] answer = new int[list.size()];
+        // System.out.println(list);
+        
         for(int i=0; i<list.size(); i++){
             answer[i] = list.get(i);
         }
