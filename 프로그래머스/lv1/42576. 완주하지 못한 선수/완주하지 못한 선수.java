be7c51ce73cc10f3b0a4960f574e347724da6f46
suuuinkim <78335072+suuuinkim@users.noreply.github.com>
2023-03-24 13:20:09 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 100.14 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,6 +1,8 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        
+        // participant - completion
         String answer = "";
         
         HashMap<String, Integer> map = new HashMap<>();
@@ -9,21 +11,19 @@ class Solution {
             map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
         }
         
-        
+        // System.out.println(map);
         
         for(int i=0; i<completion.length; i++){
-            if(map.get(completion[i]) != 0){
-                map.put(completion[i], map.get(completion[i]) -1);
+            if(map.get(completion[i]) != 0){ // map에 담겨 있으면
+                map.put(completion[i], map.get(completion[i]) - 1);
             }
         }
         
-        // System.out.println(map);
-        
-    
         for(String key : map.keySet()){
-            if(map.get(key) != 0) answer = key;
+            if(map.get(key) != 0){
+                answer = key;
+            }
         }
-        
         return answer;
     }
 }
