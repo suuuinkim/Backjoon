b9440d0bc9b12bc501b161d460e35f9ceaa41708
suuuinkim <78335072+suuuinkim@users.noreply.github.com>
2023-03-26 12:03:27 +0900

    [level 2] Title: 주식가격, Time: 13.46 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,12 +1,10 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        int cnt =0;
         for(int i=0; i<prices.length; i++){
             for(int j=i+1; j<prices.length; j++){
                 answer[i]++;
-                if(prices[i] > prices[j]) break;   
+                if(prices[i] > prices[j]) break;
             }
         }
         return answer;
