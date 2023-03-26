2ee0e987180e2055f1486d53b904687bbae5fd6d
suuuinkim <78335072+suuuinkim@users.noreply.github.com>
2023-03-26 12:31:29 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.16 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -7,36 +7,34 @@ class Solution {
         
         for(int i=0; i<truck_weights.length; i++){
             int truck = truck_weights[i];
-            
             while(true){
-                // 1. 다리에 트럭이 하나도 없을 떄
+                // 1. 트럭이 다리에 한 대도 없을 때
                 if(q.isEmpty()){
                     q.offer(truck);
                     sum += truck;
                     answer++;
                     break;
-                // 2. 다리에 트럭이 bridge_length 만큼 있을 때    
-                }else if(q.size() == bridge_length) sum -= q.poll();
-                else{
-                    // 3. 다리에 트럭이 있고 weight 보다 무게가 같거나 적을때
-                    if(sum + truck <= weight){
+                // 2. 트럭이 다리 길이 만큼 있을 때
+                }else if(q.size() == bridge_length) {
+                    sum -= q.poll();
+                }else{
+                    // 3. 다리에 트럭이 있고 무게가 weight 보다 적을 떄
+                    if(sum + truck<= weight){
                         q.offer(truck);
                         sum += truck;
                         answer++;
                         break;
-                    // 4. 다리에 트럭이 있고 weight 보다 무게가 많이 나갈 때
+                    // 4. 다리에 트럭이 있고 무게가 weight 보다 클 때
                     }else{
                         q.offer(0);
                         answer++;
+                        
                     }
-                    
-                }    
-            
+                }
             }
         }
         
         answer += bridge_length;
-        
         return answer;
     }
 }
